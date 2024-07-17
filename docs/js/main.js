c388941c141694978ea77c3b38f0a825cebb20ef
Raquel Castro <raquelcastrobermudo@gmail.com>
2024-07-17 11:55:56 +0200

    Deployed new version of the page to production

---
+++ docs/js/main.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("html"),s=document.querySelector("#toggle"),o=document.querySelector("#toggle-sm");t.classList.contains("dark")&&t.classList.remove("dark");const e=()=>{t.classList.contains("dark")?t.classList.remove("dark"):t.classList.add("dark")};s.addEventListener("click",e);o.addEventListener("click",e);
+//# sourceMappingURL=main.js.map
